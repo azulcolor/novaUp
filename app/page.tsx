3265fad997e776220bdcd52d15f7e886b94e18ab
gloverProgramando <gloverotaku@outlook.es>
2023-07-26 12:34:40 -0500

    home finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Card from "@/components/common/Card"
-import { apiRequest } from "@/libs/axios-api";
+import { api, apiRequest } from "@/libs/axios-api";
 import Carousel from "@/components/common/Carousel";
 import { IPost } from "@/interfaces";
 import CarouselCard from "@/components/common/CardCarousel";
@@ -9,14 +9,20 @@ interface Props {
 
 export default async function Home(props: Props) {
   const posts = await apiRequest.getPosts('');
-  const latests = await apiRequest.getPosts('');
+  const latests = await apiRequest.getPostsLatest(5);
+  const pinned = await apiRequest.getPostsPinned();
+
   console.log(posts);
   return (
     <main className="">
       <div className='carousel'>
-        <Carousel items={latests}/>
+        <Carousel items={latests} />
       </div>
       <div className='bottom__layer'>
+        {pinned.length &&
+          pinned.map((post: IPost, index: any) => (
+            <Card key={`${post.id}-${index}`} post={post} />
+          ))}
       </div>
     </main>
   )
