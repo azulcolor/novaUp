4bbf121cfd130aad3c553558991cac8b28520464
Jose Guzman <joseguzmandev@gmail.com>
2023-08-06 21:03:10 -0500

    add upqroo route

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-      domains: ['*', 'localhost', 'nova-up-api-development-773d.up.railway.app'],
+      domains: [
+         '*',
+         'localhost',
+         'servicios.upqroo.edu.mx',
+         'nova-up-api-development-773d.up.railway.app',
+      ],
    },
 };
 
