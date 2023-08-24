cc88d92a13376f7a68d9b34aa5fd7de8f22f6f59
Jose Guzman <joseguzmandev@gmail.com>
2023-08-23 22:50:13 -0500

    fix image no load

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
+//Debemos agregar esta configuración para que las imagenes se puedan mostrar en el servidor de producción
 const nextConfig = {
    images: {
       domains: [
          '*',
-         'localhost',
-         'servicios.upqroo.edu.mx',
-         'nova-up-api-development-773d.up.railway.app',
-         'www.youtube.com',
-         'img.youtube.com',
+         'localhost', // dominio base de el front end en desarrollo
+         'servicios.upqroo.edu.mx', // dominio base de el back end
+         'nova-up-test.vercel.app', // dominio base de el front end
+         'www.youtube.com', // dominio para mostrar recursos de youtube
+         'img.youtube.com', // dominio para mostrar recursos de youtube (imagenes)
       ],
    },
 };
